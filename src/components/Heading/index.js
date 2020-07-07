e234213f57bd792d9979f01db9cca1618b5c1cc2
Jasmine <jasmine.oji@cricket.com.au>
2020-07-07 12:39:11 +1000

    Fetch data from the api

---
+++ src/components/Heading/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Heading = ({ headingData }) => {
+  return <div>Heading Content</div>
+}
+
+export default Heading
--- src/containers/Article/index.js
+++ src/containers/Article/index.js
@@ -1,11 +1,14 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import Heading from "../../components/Heading"
+import Content from "../../components/Content"
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
 import styled from "styled-components"
 
 const ContentWrapper = styled.div`
-  padding: 0 20px;
   flex: 1;
+  padding: 0 1rem;
 `
 
 const ArticleWrapper = styled.div`
@@ -14,11 +17,45 @@ const ArticleWrapper = styled.div`
   flex-direction: column;
 `
 
+const endpoint = "http://localhost:3000/api/"
+
 const Article = () => {
+  const [headingData, updateHeadingData] = useState({
+    title: "",
+    summary: "",
+    urlPartial: "",
+    imageList: {},
+  })
+
+  const [blocks, updateBlocks] = useState([])
+
+  const getArticle = async (articleName) => {
+    const {
+      data: {
+        result: { article },
+      },
+    } = await axios.get(`${endpoint}${articleName}`)
+
+    updateHeadingData({
+      title: article.title,
+      summary: article.summary,
+      imageList: article.imageList,
+    })
+
+    updateBlocks(article.blocks)
+  }
+
+  useEffect(() => {
+    getArticle("article.json")
+  }, [])
+
   return (
     <ArticleWrapper>
       <Header />
-      <ContentWrapper>Content Placeholder</ContentWrapper>
+      <ContentWrapper>
+        <Heading headingData={headingData} />
+        <Content blocks={blocks} />
+      </ContentWrapper>
       <Footer />
     </ArticleWrapper>
   )
